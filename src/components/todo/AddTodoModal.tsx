7ccf4da4a971a154aa695520cb74c4f27c2dee81
kamrul hasan <kamrul444216@gmail.com>
2024-01-13 00:49:58 +0600

    Add: Add todo modal

--- src/components/todo/AddTodoModal.tsx
+++ src/components/todo/AddTodoModal.tsx
@@ -1,9 +1,10 @@
+import { FormEvent, useState } from "react";
 import { Button } from "../ui/button";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
@@ -12,37 +13,57 @@ import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 
 const AddTodoModal = () => {
+  const [task, setTask] = useState("");
+  const [description, setDescription] = useState("");
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log({ task, description });
+  };
+
   return (
     <Dialog>
       <DialogTrigger asChild>
-      <Button className="bg-primary-gradient text-xl font-semibold">
+        <Button className="bg-primary-gradient text-xl font-semibold">
           Add todo
         </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+          <DialogTitle className="text-center">Add Task</DialogTitle>
+          <DialogDescription className="text-center">
+            Add your task that you want to finished.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+        <form onSubmit={onSubmit}>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="task" className="text-right">
+                Task
+              </Label>
+              <Input
+                onBlur={(e) => setTask(e.target.value)}
+                id="task"
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="description" className="text-right">
+                Description
+              </Label>
+              <Input
+                onBlur={(e) => setDescription(e.target.value)}
+                id="description"
+                className="col-span-3"
+              />
+            </div>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
+          <div className="flex justify-end">
+            <DialogClose asChild>
+              <Button type="submit">Save</Button>
+            </DialogClose>
           </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
